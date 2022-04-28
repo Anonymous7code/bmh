5d1f07bf6e112c8299823f51a5862a31251b4c4b
RizviSadiya <63709880+RizviSadiya@users.noreply.github.com>
2022-04-28 13:33:24 +0530

    radiology

--- src/app/pages/radiology/radiology-home/radiology-home.component.ts
+++ src/app/pages/radiology/radiology-home/radiology-home.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-radiology-home',
   templateUrl: './radiology-home.component.html',
   styleUrls: ['./radiology-home.component.scss']
 })
 export class RadiologyHomeComponent implements OnInit {
+  radiology: any;
+  LabDetails: any;
+  constructor(private _ApiService: ApiService) { 
+    this.LabsData();
+  }
 
-  constructor() { }
+  
+  LabsData() {
+    this._ApiService.GetLabs().subscribe((res) => {
+      this.LabDetails = res;
+      console.log(this.LabDetails);
+this.radiology= this.LabDetails.filter(res=>res.lab_type=='radiology')
+console.log("this.radiology",this.radiology);
+    });
+  }
 
   ngOnInit(): void {
   }
